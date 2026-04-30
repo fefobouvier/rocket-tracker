9a40f9a28130b0ad4816f6f629e00d47dff82fe7
fefobouvier <fefobouvier@gmail.com>
2026-04-29 23:52:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -159,4 +159,4 @@ for l in launches:
         else:
             st.write(L["low_match_desc"])
 
-        st.link_button(L["btn_details"], f"https://nextspaceflight.com/launches/details/{l.get('id')}")
+        st.link_button(L["btn_details"], f"https://nextspaceflight.com/")
